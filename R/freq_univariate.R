e6abff92d2b6e7209d167b7186d79c79f0cf2cc4
mat_mayerl <mayerl@posteo.org>
2023-10-24 15:15:25 +0200

    add documentation

--- R/freq_univariate.R
+++ R/freq_univariate.R
@@ -1,36 +1,55 @@
 ## Helper Function Univariate
 
+#' Helper Function for Univariate Tables
+#' Create a simple frequency table.
+#'
+#' @param df
+#' @param var
+#'
+#' @return data.table
+#' @export
+#'
+#' @examples
 freq_univariate_helper <- function(df, var){
-  tab_n <- df[, .(n = .N), by = var]
-  tab_n[, perc := round((n/sum(n))*100, 1)]
-  tab_n[!is.na(get(var)), perc_valid := round((n/sum(n))*100, 1)]
-  tab_n[, variable := var]
-  setnames(tab_n, old = var, new = "category")
-  tab_n[, category := fct_na_value_to_level(category, level = "(Missing)")]
-  tab_n[, id_group := seq.int(nrow(tab_n))]
+  tab_n <- df[, .(n = .N), by = var] # Calculate Frequency
+  tab_n[, perc := round((n/sum(n))*100, 1)] # Add Percent
+  tab_n[!is.na(get(var)), perc_valid := round((n/sum(n))*100, 1)] # Add valid Percent
+  tab_n[, variable := var] # Create Variablen Name
+  setnames(tab_n, old = var, new = "category") # Rename Variable to category
+  tab_n[, category := fct_na_value_to_level(category, level = "(Missing)")] # Rename Missings
+  tab_n[, id_group := seq.int(nrow(tab_n))] # Add id of groups
     #tab_n[ , c("id_group", names(tab_n)[names(tab_n) != "id_group"])]
   return(tab_n)
 }
 
 
-tableband_uni <- function(df, vars, weights = NULL){
-  label_lookup_map <- lookup_fast(df)
+#' Create a Univariate Frequency Table
+#'
+#' @param df
+#' @param vars
+#' @param weights
+#'
+#' @return
+#' @export
+#'
+#' @examples tableband_uni(df, vars = c("var1", "var2))
+tableband_uni <- function(df, vars){
+  label_lookup_map <- lookup_fast(df) # Create Lookup Table for adding Labels
   #label_lookup_map <- data.table(label_lookup_map) ## kann später entfernt werden...
-  df_list <- lapply(vars, function (x) freq_univariate_helper(df, var = x))
+  df_list <- lapply(vars, function (x) freq_univariate_helper(df, var = x)) # Apply Function on multiple Vars
 
   df_list <- rbindlist(df_list)
-  df_list <- label_lookup_map[df_list, on = c("variable")]
+  df_list <- label_lookup_map[df_list, on = c("variable")] # Add Variable Labels from Lookup Table
   #df_list[, Fragestellung_neu := NULL]
   #df_list[, id := NULL]
 
+  # Make Table pretty
   df_list[, variable := fifelse(id_group >= 2, "-", variable)]
   df_list[, variable_label := fifelse(id_group >= 2, "-", variable_label)]
   df_list[, id_group := NULL]
 
   return(df_list)
 }
-
-
 
 
 
